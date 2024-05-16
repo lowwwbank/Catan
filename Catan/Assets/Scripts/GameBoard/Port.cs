4cecee626c6e7a563f8266a30c5ffc85397da5a3
Alexander Kuznetsov <71138865+AlexKu4@users.noreply.github.com>
2024-05-16 18:47:18 +0300

    Port added

---
+++ Catan/Assets/Scripts/GameBoard/Port.cs
@@ -0,0 +1,20 @@
+using Catan.ResourcePhase;
+
+namespace Catan.GameBoard
+{
+
+    public class Port
+    {
+
+        public int toGet = 3;
+        public int toGive = 1;
+   
+        public int xCoord;
+        public int yCoord;
+   
+        public Resource.ResourceType type;
+
+    
+        public float direction = 0;
+    }
+}
