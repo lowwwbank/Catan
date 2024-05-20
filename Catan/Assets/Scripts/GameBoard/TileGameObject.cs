a798a6b99028f3e3c57174598e90443247aac040
Alexander Kuznetsov <71138865+AlexKu4@users.noreply.github.com>
2024-05-20 11:46:44 +0300

    Tile object added

---
+++ Catan/Assets/Scripts/GameBoard/TileGameObject.cs
@@ -0,0 +1,65 @@
+using Catan.GameManagement;
+using TMPro;
+using UnityEngine;
+
+namespace Catan.GameBoard
+{
+    public class TileGameObject : MonoBehaviour
+    {
+        public TextMeshProUGUI text;
+
+        public GameObject mesh;
+ 
+        public MeshRenderer tokenRenderer;
+  
+        public MeshRenderer robberRenderer;
+
+        public int diceValue;
+
+        public bool robber;
+
+        public int xIndex;
+
+        public int yIndex;
+
+        public int xCoord;
+
+        public int yCoord;
+
+        public void OnMouseDown()
+        {
+            GameObject.Find("Game Manager").GetComponent<InteractionManager>().TileClicked(this, xIndex, yIndex);
+        }
+
+        public void SetDiceValue(int value)
+        {
+            diceValue = value;
+            UpdateDiceValueText();
+        }
+
+        public void UpdateDiceValueText()
+        {
+            text.text = diceValue > 0 ? diceValue.ToString() : "";
+            tokenRenderer.enabled = diceValue > 0;
+        }
+
+        public void SetRobber(bool hasRobber)
+        {
+            robber = hasRobber;
+            UpdateRobberMesh();
+        }
+
+        public void UpdateRobberMesh()
+        {
+            robberRenderer.enabled = robber;
+        }
+
+        public void SetAppearance(Tile.TileType type)
+        {
+            mesh = Instantiate(GameObject.Find("Tile Asset Holder").transform.GetChild((int)type)).gameObject;
+            mesh.transform.position = transform.position;
+            mesh.transform.rotation = Quaternion.Euler(0, 30 + 60 * UnityEngine.Random.Range(0, 6), 0);
+            mesh.transform.parent = transform;
+        }
+    }
+}
