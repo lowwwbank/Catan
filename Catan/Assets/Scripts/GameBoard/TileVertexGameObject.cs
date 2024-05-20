42729b9c14d4f3ca56d5d2f764ac7fdc4ac91359
Alexander Kuznetsov <71138865+AlexKu4@users.noreply.github.com>
2024-05-20 21:22:37 +0300

    Object tile vertex

---
+++ Catan/Assets/Scripts/GameBoard/TileVertexGameObject.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using Catan.Players;
+
+namespace Catan.GameBoard
+{
+    public class TileVertexGameObject : BoardTokenGameObject
+    {
+        public GameObject port;
+  
+        public GameObject village;
+  
+        public GameObject city;
+
+        public override void SetPlayer(Player player)
+        {
+            base.SetPlayer(player);
+            port.GetComponent<Renderer>().material.color = player.playerColor;
+            city.GetComponent<Renderer>().material.color = player.playerColor;
+        }
+
+        public override void ResetBoardTokenObject()
+        {
+            base.ResetBoardTokenObject();
+            city.GetComponent<Renderer>().material.color = Color.white;
+            port.GetComponent<MeshRenderer>().enabled = false;
+            UpdateMesh();
+        }
+
+        public void UpdateMesh()
+        {
+            switch (GameObject.Find("Board").GetComponent<Board>().vertices[xIndex][yIndex].development)
+            {
+                case TileVertex.Development.Undeveloped:
+                    meshRenderer.enabled = false;
+                    city.GetComponent<MeshRenderer>().enabled = false;
+                    break;
+                case TileVertex.Development.Town:
+                    meshRenderer.enabled = true;
+                    city.GetComponent<MeshRenderer>().enabled = false;
+                    break;
+                case TileVertex.Development.City:
+                    meshRenderer.enabled = false;
+                    city.GetComponent<MeshRenderer>().enabled = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void Start()
+        {
+            if (GameObject.Find("Board").GetComponent<Board>().vertices[xIndex][yIndex].up)
+            {
+                int rotation = Random.Range(0, 3);
+                village.transform.rotation = Quaternion.Euler(0, 60 + rotation * 120, 0);
+                city.transform.rotation = Quaternion.Euler(0, 60 + rotation * 120, 0);
+            }
+            else
+            {
+                int rotation = Random.Range(0, 3);
+                village.transform.rotation = Quaternion.Euler(0, rotation * 120, 0);
+                city.transform.rotation = Quaternion.Euler(0, rotation * 120, 0);
+            }
+        }
+    }
+}
